38c159e6fcd51e6b95de2fe674d09658c7e25c50
jlim128 <limjonathan5810@students.esuhsd.org>
2020-02-01 19:33:26 -0800

    doors work

---
+++ Assets/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public BoxCollider2D boxCollider2D;
+    private bool checkE;
+    public float progress;
+    public float maxProgress = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(progress <= maxProgress){
+            // fillController.SetFill(progress);
+            if(checkE){
+                progress += 0.5f;// * Time.deltaTime;
+            }
+        }
+        else if(progress > maxProgress){
+            boxCollider2D.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            checkE = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player"))
+            checkE = false;
+    }
+}
